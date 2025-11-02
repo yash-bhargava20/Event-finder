b5e1c593a4cd92b41f30c807c4fe7d3a7136edbd
yash-bhargava20 <bhargavayash94@gmail.com>
2025-11-02 10:35:35 +0530

    event routes

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const router = express.Router();
 const {
   getEvents,
   createEvent,
@@ -9,3 +11,4 @@ router.get("/", getEvents);
 router.get("/:id", getEventById);
 router.post("/", createEvent);
 router.delete("/:id", deleteEvent);
+module.exports = router;
